6acb1ed1f18d8aa38945b7ec80212ddb2f73d1bd
Jose <jotangamers@gmail.com>
2024-07-20 16:43:59 -0400

    Avanzando algunas funciones JS para la seccion de comentarios

--- comentario.js
+++ comentario.js
@@ -1,16 +1,41 @@
-const hamButton = document.getElementById('hamButton');
-const nav = document.querySelector('.nav');
+// Asegurándome que primero cargue el DOM antes de que se ejecute el código JS
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Funcionalidades del botón hamburguesa
+    const hamButton = document.getElementById('hamButton');
+    const nav = document.querySelector('.nav');
 
-hamButton.addEventListener('click', function() {
-    if (window.innerWidth < 600) {
-        nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
+    if (hamButton && nav) {
+        hamButton.addEventListener('click', function () {
+            if (window.innerWidth < 600) {
+                nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
+            }
+        });
+
+        window.addEventListener('resize', function () {
+            if (window.innerWidth >= 600) {
+                nav.style.display = 'flex';
+            } else {
+                nav.style.display = 'none';
+            }
+        });
+
+        // Asegurarse de que el estado inicial del menú es correcto
+        if (window.innerWidth < 600) {
+            nav.style.display = 'none';
+        } else {
+            nav.style.display = 'flex';
+        }
     }
-});
 
-window.addEventListener('resize', function() {
-    if (window.innerWidth >= 600) {
-        nav.style.display = 'flex';
-    } else {
-        nav.style.display = 'none';
+    // Ajustar el cursor del input 
+    const comentarioInput = document.getElementById('comentarioInput');
+    const maxLength = 150; 
+
+    if (comentarioInput) {
+        comentarioInput.addEventListener('input', () => {
+            if (comentarioInput.value.length > maxLength) {
+                comentarioInput.value = comentarioInput.value.substring(0, maxLength);
+            }
+        });
     }
 });
